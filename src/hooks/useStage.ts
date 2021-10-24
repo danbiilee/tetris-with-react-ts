3637f925c22293229dc441f26b9d7b5b27809cf1
dblee <danbi.db@gmail.com>
2021-10-24 10:45:38 +0900

    Add clear rows

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -10,14 +10,35 @@ export type STAGE = STAGECELL[][];
 
 export const useStage = (player: Player, resetPlayer: () => void) => {
   const [stage, setStage] = React.useState(createStage());
+  const [rowsCleared, setRowsCleared] = React.useState(0);
 
   React.useEffect(() => {
     if (!player.pos) return;
 
+    setRowsCleared(0);
+
+    const sweepRows = (newStage: STAGE): STAGE => {
+      return newStage.reduce((ack, row) => {
+        // If we don't find a 0, it means that the row is full and should be cleared
+        if (row.findIndex((cell) => cell[0] === 0) === -1) {
+          setRowsCleared((prev) => prev + 1);
+          // Create an empty row at the beginning of the array to push the Tetrominos down
+          // instead of returning the cleared row
+          ack.unshift(
+            new Array(newStage[0].length).fill([0, "clear"]) as STAGECELL[]
+          );
+          return ack;
+        }
+
+        ack.push(row);
+        return ack;
+      }, [] as STAGE);
+    };
+
     const updateStage = (prevStage: STAGE): STAGE => {
       // First flush the stage
       // If it sasys 'clear' but don't have a 0, it means that it's the players move and should be cleared.
-      const newSTage = prevStage.map(
+      const newStage = prevStage.map(
         (row) =>
           row.map((cell) =>
             cell[1] === "clear" ? [0, "clear"] : cell
@@ -29,7 +50,7 @@ export const useStage = (player: Player, resetPlayer: () => void) => {
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
-            newSTage[y + player.pos.y][x + player.pos.x] = [
+            newStage[y + player.pos.y][x + player.pos.x] = [
               value,
               `${player.collided ? "merged" : "clear"}`,
             ];
@@ -37,7 +58,12 @@ export const useStage = (player: Player, resetPlayer: () => void) => {
         });
       });
 
-      return newSTage;
+      if (player.collided) {
+        resetPlayer();
+        return sweepRows(newStage);
+      }
+
+      return newStage;
     };
 
     setStage((prev) => updateStage(prev));
